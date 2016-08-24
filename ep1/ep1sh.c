d5fafbbf2e8f0bbeed40445f3e4ebbef85fa60c1
Gabriel Russo <gabrielrcarmo@gmail.com>
2016-08-24 14:56:29 -0300

    arrumando bug de input vazio

--- ep1/ep1sh.c
+++ ep1/ep1sh.c
@@ -29,26 +29,30 @@ int main (int argc, char **argv) {
             params[i++] = p;
             p = strtok (NULL, " ");
         }
-        command = params[0];
         params[i] = NULL;
+        command = params[0];
 
         /* Processa o input */
-        if (strcmp (command, "exit") == 0) {
-            exit (EXIT_SUCCESS);
-        } else if (strcmp (command, "chmod") == 0) {
-            chmod (params[2], strtol (params[1], NULL, 8));
-        } else if (strcmp (command, "id") == 0) {
-            if (strcmp (params[1], "-u") == 0) printf ("%d\n", getuid ());
-        } else {
-            switch (fork ()) {
-                case -1:
-                    fprintf (stderr, "ERRO\n");
-                    exit (EXIT_FAILURE);
-                case 0:
-                    status = execve (command, params, 0);
-                    exit (status);
-                default:
-                    waitpid (-1, &status, 0);
+        if (command != NULL) {
+            if (strcmp (command, "exit") == 0) {
+                free (input);
+                free (path);
+                exit (EXIT_SUCCESS);
+            } else if (strcmp (command, "chmod") == 0) {
+                chmod (params[2], strtol (params[1], NULL, 8));
+            } else if (strcmp (command, "id") == 0) {
+                if (strcmp (params[1], "-u") == 0) printf ("%d\n", getuid ());
+            } else {
+                switch (fork ()) {
+                    case -1:
+                        perror ("Error");
+                    case 0:
+                        status = execve (command, params, 0);
+                        if (status) perror ("Error");
+                        exit (status);
+                    default:
+                        waitpid (-1, &status, 0);
+                }
             }
         }
 
