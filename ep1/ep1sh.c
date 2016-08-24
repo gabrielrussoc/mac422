6b7053cde84187b1e96d813541afdaeded750f3c
Gabriel Russo <gabrielrcarmo@gmail.com>
2016-08-24 14:31:35 -0300

    polindo e comentando - memory leak??

--- ep1/ep1sh.c
+++ ep1/ep1sh.c
@@ -5,33 +5,24 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 
 #define MAX 1024
 #define TRUE 1
 
-int octa (char *s) {
-    int k = atoi(s);
-    int pot = 1, ret = 0;
-    while (k) {
-        ret += pot*(k%10);
-        k /= 10;
-        pot *= 8;
-    }
-    return ret;
-}
-
 int main (int argc, char **argv) {
-    char path[MAX], prompt[MAX], *input;
+    char prompt[MAX], *input, *path;
     char *params[MAX], *command, *p;
     int status, i;
-    pid_t pid;
     
     while (TRUE) {
-        getcwd (path, MAX);
+        /* Exibe o prompt e le o input do usuario */
+        path = getcwd (NULL, 0);
         sprintf (prompt, "(%s): ", path);
         input = readline (prompt);
         add_history (input);
 
+        /* Separa o input em comando e parametros */
         i = 0;
         p = strtok (input, " ");
         while (p != NULL) {
@@ -39,25 +30,33 @@ int main (int argc, char **argv) {
             p = strtok (NULL, " ");
         }
         command = params[0];
-        params[i] = 0;
+        params[i] = NULL;
 
-        if (strcmp (command, "chmod") == 0) {
-            chmod (params[2], octa(params[1]));
+        /* Processa o input */
+        if (strcmp (command, "exit") == 0) {
+            exit (EXIT_SUCCESS);
+        } else if (strcmp (command, "chmod") == 0) {
+            chmod (params[2], strtol (params[1], NULL, 8));
         } else if (strcmp (command, "id") == 0) {
-            printf ("%d\n", getuid ());
+            if (strcmp (params[1], "-u") == 0) printf ("%d\n", getuid ());
         } else {
-            switch (pid = fork()) {
+            switch (fork ()) {
                 case -1:
                     fprintf (stderr, "ERRO\n");
-                    exit(EXIT_FAILURE);
+                    exit (EXIT_FAILURE);
                 case 0:
                     status = execve (command, params, 0);
                     exit (status);
                 default:
-                    waitpid (pid, &status, 0);
+                    waitpid (-1, &status, 0);
             }
         }
+
+        /* Limpa a memoria */
+        free (input);
+        free (path);
     }
     
-    return 0;
+    return EXIT_SUCCESS;
 }
+
