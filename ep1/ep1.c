fdc207c02a947e69477b0d8ad75177489b8a9182
Victor Wichmann Raposo <vwraposo@gmail.com>
2016-09-06 21:47:52 -0300

    Adicionando arquivo de saida e inserindo detalhes

--- ep1/ep1.c
+++ ep1/ep1.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "schedule.h"
 
 int main (int argc, char **argv) {
-    FILE *in = fopen (argv[1], "r");
-    fcfs (in);
+    int scheduler = atoi (argv[1]);
+    FILE *in = fopen (argv[2], "r");
+    FILE *out = fopen (argv[3], "w");
+    switch (scheduler) {
+        case 1:
+            fcfs (in, out);
+            break;
+        default:
+            printf ("Esse nao e um escalonador valido\n");
+            break;
+    }
+
+    return 0;
 }
 
--- ep1/schedule.c
+++ ep1/schedule.c
@@ -5,48 +5,6 @@
 #include <unistd.h>
 #include <pthread.h>
 
-/* Variaveis globais */
-pthread_mutex_t thread_lock;    
-pthread_mutex_t queue_lock;
-Queue q;
-int n_cpu;
-int n_thread;
-
-void thread_wake (Process p) {
-    pthread_mutex_lock (&thread_lock);
-    p->paused = 0;
-    pthread_cond_signal (&(p->cond));
-    pthread_mutex_unlock (&thread_lock);
-}
-
-void thread_sleep (Process p) {
-    pthread_mutex_lock (&thread_lock);
-    p->paused = 1;
-    pthread_mutex_unlock (&thread_lock);
-}
-
-/* Trava a thread do processo p caso esta esteja pausada ou nao faz nada */
-static void thread_check (Process p) {
-    pthread_mutex_lock (&thread_lock);
-    while (p->paused)
-        pthread_cond_wait (&(p->cond), &thread_lock);
-    pthread_mutex_unlock (&thread_lock);
-}
-
-/* Escolhe a proxima thread a ser executada no algoritmo FCFS */
-static void next_fcfs () {
-    Process p;
-    pthread_mutex_lock (&queue_lock);
-    if (n_thread < n_cpu && !queue_isempty (q)) {
-        p = queue_front (q);
-        printf ("Rodei %s com %lf\n", p->name, elapsed());
-        thread_wake (queue_front (q));
-        dequeue (q);
-        n_thread++;
-    } 
-    pthread_mutex_unlock (&queue_lock);
-}
-
 void *do_something (void *a) {
     Process p = a;
     int foo = 1;
@@ -65,13 +23,34 @@ void *do_something (void *a) {
     n_thread--;
     pthread_mutex_unlock (&queue_lock);
 
+
+    aux = elapsed ();
+    fprintf (g_out, "%s %lf %lf\n", p->name, aux, aux - p->t0);
     printf ("%s acabou com %lf\n", p->name, elapsed ());
     
     next_fcfs ();
     pthread_exit (NULL);
 }
 
-void fcfs (FILE *in) {
+/* Escalonador FCFS ////////////////////////////////////////////
+///////////////////////////////////////////////////////////// */
+
+/* Escolhe a proxima thread a ser executada no algoritmo FCFS */
+static void next_fcfs () {
+    Process p;
+    pthread_mutex_lock (&queue_lock);
+    if (n_thread < n_cpu && !queue_isempty (q)) {
+        p = queue_front (q);
+        printf ("Rodei %s com %lf\n", p->name, elapsed());
+        thread_wake (queue_front (q));
+        dequeue (q);
+        n_thread++;
+    } 
+    pthread_mutex_unlock (&queue_lock);
+}
+
+void fcfs (FILE *in, FILE *out) {
+    g_out = out;
     Process next;
     int i = 0;
     pthread_t tid[123];
@@ -97,9 +76,14 @@ void fcfs (FILE *in) {
     }
 
     while (i) pthread_join(tid[--i], NULL);
+
+    fprintf ("%d\n", g_context);
 }
 
-double elapsed () {
-    return (double) clock () / CLOCKS_PER_SEC;
-}
+/* Escalonador SRTN ////////////////////////////////////////////
+///////////////////////////////////////////////////////////// */
+
+/* Escalonador Multiplas Filas  ////////////////////////////////
+///////////////////////////////////////////////////////////// */
+
 
