5940f37722cee2a80d620d569f50e664139647f1
Victor Wichmann Raposo <vwraposo@gmail.com>
2016-09-06 21:52:28 -0300

    Modo debug

--- ep1/ep1.c
+++ ep1/ep1.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "schedule.h"
+#include "utility.h"
 
 int main (int argc, char **argv) {
     int scheduler = atoi (argv[1]);
     FILE *in = fopen (argv[2], "r");
     FILE *out = fopen (argv[3], "w");
+
+    if (argc == 5 && strcmp (argv[5], "d") == 0) g_debug = 1;
     switch (scheduler) {
         case 1:
             fcfs (in, out);
