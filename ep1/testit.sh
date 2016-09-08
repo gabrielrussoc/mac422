a69d9f5b57784fdcd2d40ab0790ff139fae401c5
Gabriel Russo <gabrielrcarmo@gmail.com>
2016-09-08 12:07:25 -0300

    fix no tester

--- ep1/testit.sh
+++ ep1/testit.sh
@@ -1,5 +1,6 @@
-for type in small; do
-    for i in `seq 1 1`; do
+for type in small medium large; do
+    for i in `seq 1 30`; do
+        echo $type $i
         ./ep1 1 test_data/$type/$type.in test_data/$type/fcfs/$type$i.out &
         ./ep1 2 test_data/$type/$type.in test_data/$type/srtn/$type$i.out &
         ./ep1 3 test_data/$type/$type.in test_data/$type/multi/$type$i.out
