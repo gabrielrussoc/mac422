5dbf3ab42e17df5da8408e557554ce46f8b8cd79
Gabriel Russo <gabrielrcarmo@gmail.com>
2016-09-09 16:49:30 -0300

    agora sim python eh o poder

--- ep1/test_data/disseca.py
+++ ep1/test_data/disseca.py
@@ -14,13 +14,13 @@ for size in ['small', 'medium', 'large']:
         print("Escalonador: "+sched)
         os.chdir(sched)
         write = open('../../../'+pessoa+'-'+size+'-'+sched+'.csv', 'w')
-        write.write('deadline,context')
+        write.write('deadline,context\n')
         for out in glob.glob("*.out"):
             print(out)
             loc = list(open(out, 'r'))
             deadline = (loc[-1].split())[0] #deadline
             context = (loc[-2].split())[0] #context
-            write.write(deadline+','+context)
+            write.write(deadline+','+context+'\n')
         write.close()
         os.chdir('..')
     os.chdir('..')
