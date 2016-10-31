92a6ce76985f085462067eda0f493ed4fd71f317
Victor Wichmann Raposo <vwraposo@gmail.com>
2016-10-31 17:23:22 -0200

    Prototipo next fit

--- ep3/espaco.py
+++ ep3/espaco.py
@@ -1,10 +1,24 @@
 def first_fit (p_size):
-    arr = [0] * p_size
-    for i in bitmap:
+    arr = [False] * p_size
+    for i in range (0, len (bitmap)-p_size):
         if bitmap[i:i+p_size] == arr:
             #insere no arquivo 
-            bitmap[i:i+p_size] = [1] * p_size
+            bitmap[i:i+p_size] = [True] * p_size
             break;
 
+
+def next_fit (p_size):
+    arr = [False] * p_size
+    i = next_fit.counter + 1
+    while (i != next_fit.counter):
+        if bitmap[i:i+p_size] == arr:
+            #insere no arquivo 
+            bitmap[i:i+p_size] = [True] * p_size
+            next_fit.counter = i + p_size - 1
+            break;
+
+        
+
+def best_fit (p_size):
 
             
