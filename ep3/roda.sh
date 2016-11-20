284f85ec1c9173c28586493da8d5cefa5d56ffdf
Gabriel Russo <gabrielrcarmo@gmail.com>
2016-11-20 00:41:52 -0200

    script para rodar o ep

---
+++ ep3/roda.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/zsh
+
+# Parametros da linha de comando
+# Caso de teste $1
+# Algoritmo de gerencia de memoria $2
+# Algoritmo de substituicao de paginas $3
+# Intervalo de prints na tela $4
+#
+
+all='carrega tests/'$1'.in\nespaco '$2'\nsubstitui '$3'\nexecuta '$4'\nsai'
+echo $all | python3 ep3.py
+
