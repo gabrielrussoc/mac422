b87a9183cefebdf4b49a44e67bc60ee878e3ec6b
Gabriel Russo <gabrielrcarmo@gmail.com>
2016-11-20 17:05:56 -0200

    comentando o gerador do marcos pontes

--- ep3/trace_generator.py
+++ ep3/trace_generator.py
@@ -13,32 +13,33 @@ l_total = 1000 # ?
 v_total = 1000 # Valor maximo para memoria virtual
 t_final = 10000 # Tempo final maximo
 
-f = open('in', 'w')
+f = open('tests/big.in', 'w')
 
-s = random.randint(1, s_total)
-p = random.randint(1, p_total) * s
-virtual = random.randint(2, v_total) * p
-total = random.randint(1, virtual / p / 2) * p
-size = random.randint(1, 100)
+#s = random.randint(1, s_total)
+s = 1
+p = random.randint(1, p_total) * s #tamanho da pagina
+virtual = random.randint(2, v_total) * p #tamanho da memoria virtual
+total = random.randint(1, int (virtual / p / 2)) * p #tamanho da memoria real (no maximo metade da virtual)
+size = random.randint(1, 100) #quantidade de processos no trace
 
 f.write(str(total) + ' ' + str(virtual) + ' ' + str(s) + ' ' + str(p) + '\n')
 
 tant = 0
 sm = 0
 for i in range(0, size):
-	t0 = random.randint(tant, t_final)
-	tf = random.randint(t0, t_final * 2)
-	sz = random.randint(0, min(tf - t0, 100))
-	nome = 'processo' + str(i + 1)
-	b = random.randint(2, int(virtual / size))
-	if (math.ceil(b/p) * p > virtual / size):
-		b = b - p
-	sm = sm + math.ceil(b / p) * p
+	t0 = random.randint(tant, t_final) #t0 
+	tf = random.randint(t0, t_final * 2) #tf
+	sz = random.randint(0, min(tf - t0, 100)) #numero de acessos do processo
+	nome = 'processo' + str(i + 1) #nome
+	b = random.randint(1, int(virtual / size)) #tamanho do processo
+	if (math.ceil(b/p) * p > virtual / size): # caso o processo ocupe uma pagina a mais
+		b = b - p 
+	sm = sm + math.ceil(b / p) * p #quanto ja usamos ate agora
 	f.write(str(t0) + ' ' + nome + ' ' + str(tf) + ' ' + str(b))
 	ti = []
 	pi = []
 	for k in range(0, sz):
-		pi.append(random.randint(1, b - 1))
+		pi.append(random.randint(0, b - 1))
 		ti.append(random.randint(t0, tf))
 	ti.sort()
 	for k in range(0, sz):
@@ -48,8 +49,4 @@ for i in range(0, size):
 f.close()
 #if (virtual < sm):
 #	print("Deu bosta " + str(virtual) + ' < ' + str(sm))
-
-
-
-
 
